444f52afe70418ff9eeb3691afb0cc0c9d901422
古瀬　就悟 <shugoena@gmail.com>
2020-09-27 02:47:05 +0900

    commit

--- app/controllers/license_groups_controller.rb
+++ app/controllers/license_groups_controller.rb
@@ -4,8 +4,21 @@ class LicenseGroupsController < ApplicationController
     @license_group = LicenseGroup.find(params[:license_group_id])
   end
   def download2
-    pdf = RecordPdf.new()
+    pdf = Prawn::Document.new(:page_size => 'A4', :top_margin => 0, :bottom_margin => 0, :left_margin => 0, :right_margin => 0)
+    pdf.text "Hello, world!!"
+    pdf.fill_rectangle [40-36, 161.5-36], 243.6, 153
+    pdf.fill_rectangle [40-36, 329.5-36], 243.6, 153
+    pdf.fill_rectangle [40-36, 329.5-36], 243.6, 153
+    pdf.fill_rectangle [40-36, 497.5-36], 243.6, 153
+    pdf.fill_rectangle [40-36, 665.5-36], 243.6, 153
+    pdf.fill_rectangle [40-36, 833.5-36], 243.6, 153
 
-    send_data pdf.render, filename: "sample.pdf", type: "application/pdf", disposition: "inline" # 画面に表示。外すとダウンロードされる。
+    pdf.fill_rectangle [312.6-36, 161.5-36], 243.6, 153
+    pdf.fill_rectangle [312.6-36, 329.5-36], 243.6, 153
+    pdf.fill_rectangle [312.6-36, 329.5-36], 243.6, 153
+    pdf.fill_rectangle [312.6-36, 497.5-36], 243.6, 153
+    pdf.fill_rectangle [312.6-36, 665.5-36], 243.6, 153
+    pdf.fill_rectangle [312.6-36, 833.5-36], 243.6, 153
+    send_data pdf.render, filename: "sample.pdf", type: "application/pdf", disposition: "inline"
   end
 end
