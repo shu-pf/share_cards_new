63d75fa4c19651ceb77b4e80604f9954bfdb7aae
shu-pf <shugoena@gmail.com>
2020-10-11 22:46:22 +0900

    調整

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
   def index
-    redirect_to "https://www.share-cards.com/" if root_url = "https://share-cards.herokuapp.com/"
+    redirect_to "https://www.share-cards.com/", status: 301 if root_url == "https://share-cards.herokuapp.com/"
   end
   def topic
   end
