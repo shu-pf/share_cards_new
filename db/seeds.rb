b6615098f5e254191d0756367cea45cb9fd9a64e
shu-pf <shugoena@gmail.com>
2020-10-23 01:33:25 +0900

    変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,6 @@ User.create!(username:  "管理者",
   email: ENV['ADMIN_EMAIL'],
   password:  ENV['ADMIN_PASSWORD'],
   password_confirmation: ENV['ADMIN_PASSWORD'],
-  admin: true)
+  admin: true,
+  creator: true,
+  validate: false)
